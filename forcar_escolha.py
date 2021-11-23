2bd126ed178de2d9a5733188a6cff9ad8425fba9
Vinícius Costa Souto <vinicius.soutto@gmail.com>
2021-11-23 18:09:55 -0300

    Atualização com um novo arquivo incluindo a função de forçar escolha resumida

---
+++ forcar_escolha.py
@@ -0,0 +1,26 @@
+def forcar_escolha(pergunta, qtde_respostas)
+    if qtde_respostas == 1:
+        while pergunta not in ("1")
+	    pergunta
+        resposta = int(pergunta)
+        return resposta
+    if qtde_respostas == 2:
+        while pergunta not in ("1", "2")
+	    pergunta
+        resposta = int(pergunta)
+        return resposta
+    if qtde_respostas == 3:
+        while pergunta not in ("1", "2", "3")
+	    pergunta
+        resposta = int(pergunta)
+        return resposta
+    if qtde_respostas == 4:
+        while pergunta not in ("1", "2", "3", "4")
+	    pergunta
+        resposta = int(pergunta)
+        return resposta
+    if qtde_respostas == 5:
+        while pergunta not in ("1", "2", "3", "4", "5")
+	    pergunta
+        resposta = int(pergunta)
+        return resposta
